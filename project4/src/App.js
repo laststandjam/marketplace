c246cf5cb0da75a4c8b3bb5411c862c177e0a8b3
ihavehands <zaneshook@gmail.com>
2020-02-23 12:11:35 -0800

    setting up componets

---
+++ project4/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./componets/Navbar";
+import Home from "./componets/Home";
+import Signup from "./componets/Signup";
+import Tickets from "./componets/Tickets";
+import User from "./componets/User";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/tickets" component={Tickets} />
+        <Route exact path="/user" component={User} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ project4/src/componets/Signup/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Navlink} from "react-router-dom"
+
+const Signup =()=>{
+  return(
+  <div>Signup</div>
+)}
+
+
+export default Signup;
